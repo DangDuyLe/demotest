966f179697da96f3a68495037f7a1f68aa2d5168
DangDuyLe <lenguyenduy1103@gmail.com>
2025-03-14 21:06:34 +0700

    Add image domains and remote patterns to Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: [
+      'ipfs.io',
+      'gateway.ipfs.io',
+      'ipfs.infura.io',
+      'nft-cdn.alchemy.com',
+      'i.seadn.io',
+      'raw.githubusercontent.com',
+      'gateway.pinata.cloud',
+      'cloudflare-ipfs.com'
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.ipfs.dweb.link',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.arweave.net',
+      }
+    ]
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       // Client-side polyfills
