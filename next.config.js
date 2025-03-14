37cc433e92ba99dfe07295323096f8c2d5dac2d9
DangDuyLe <lenguyenduy1103@gmail.com>
2025-03-14 21:19:08 +0700

    Update Next.js configuration to modify image remote patterns and add Webpack fallback for crypto-browserify

--- next.config.js
+++ next.config.js
@@ -1,26 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'ipfs.io',
-      'gateway.ipfs.io',
-      'ipfs.infura.io',
-      'nft-cdn.alchemy.com',
-      'i.seadn.io',
-      'raw.githubusercontent.com',
-      'gateway.pinata.cloud',
-      'cloudflare-ipfs.com'
-    ],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**.ipfs.dweb.link',
+        protocol: "https",
+        hostname: "api.opensea.io",
+        pathname: "/api/v1/asset/**",
       },
       {
-        protocol: 'https',
-        hostname: '**.arweave.net',
+        protocol: "https",
+        hostname: "gateway.pinata.cloud",
+        pathname: "/ipfs/**",
       }
-    ]
+    ],
+  },
+  env: {
+    ETHERSCAN_API_KEY: process.env.ETHERSCAN_API_KEY,
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
@@ -50,4 +45,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
